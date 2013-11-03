f09614f3de416a498e9c913456a37d186d561cee
Dmitriy Savchenko <login.was.here@gmail.com>
2013-11-03 17:20:02 +0200

    Removed old version of app code structure

--- assets/js/build.js
+++
@@ -1,8 +0,0 @@
-({
-    baseUrl: ".",
-    name: "vendor/almond",
-    include: "require_main",
-    mainConfigFile: "require_main.js",
-    out: "require_main.built.js",
-    findNestedDependencies: true
-})
